e796933d42ecf22f31b938cd4cbfcf230f1bd506
FangjingE <614182859@qq.com>
2023-02-27 17:15:02 +0800

    complete cart

---
+++ business_server_10300/src/main/java/com/neusoft/controller/BusinessController.java
@@ -0,0 +1,41 @@
+package com.neusoft.controller;
+
+import com.neusoft.po.Business;
+import com.neusoft.po.CommonResult;
+import com.neusoft.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/BusinessController")
+public class BusinessController {
+
+    @Autowired
+    private BusinessService businessService;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/listBusinessByOrderTypeId/{orderTypeId}")
+    public CommonResult<List> listBusinessByOrderTypeId(@PathVariable("orderTypeId") Integer
+                                                                orderTypeId) throws Exception{
+        List<Business> list = businessService.listBusinessByOrderTypeId(orderTypeId);
+        return new CommonResult(200,"success",list);
+    }
+
+    @GetMapping("/getBusinessById/{businessId}")
+    public CommonResult<Business> getBusinessById(@PathVariable("businessId") Integer
+                                                          businessId) throws Exception{
+        Business business = businessService.getBusinessById(businessId);
+        //在商家微服务中调用食品微服务
+        CommonResult<List> result =
+                restTemplate.getForObject("http://localhost:10200/FoodController/listFoodByBusinessId/"+businessId, CommonResult.class);
+        if(result.getCode()==200) {
+            business.setFoodList(result.getResult());
+        }
+        return new CommonResult(200,"success",business);
+    }
+}
