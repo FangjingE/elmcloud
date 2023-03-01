2707a893ddfcd9de4500eacb7a18687291f00892
FangjingE <614182859@qq.com>
2023-03-01 12:57:34 +0800

    complete feign

--- business_server_10300/src/main/java/com/neusoft/controller/BusinessController.java
+++ business_server_10300/src/main/java/com/neusoft/controller/BusinessController.java
@@ -1,14 +1,22 @@
 package com.neusoft.controller;
-
-import com.neusoft.po.Business;
-import com.neusoft.po.CommonResult;
-import com.neusoft.service.BusinessService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import com.neusoft.feign.FoodFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
+import com.neusoft.po.Business;
+import com.neusoft.po.CommonResult;
+import com.neusoft.po.Food;
+import com.neusoft.service.BusinessService;
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/BusinessController")
@@ -17,7 +25,12 @@ public class BusinessController {
     @Autowired
     private BusinessService businessService;
     @Autowired
-    private RestTemplate restTemplate;
+    private FoodFeignClient foodFeignClient;
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
 
     @GetMapping("/listBusinessByOrderTypeId/{orderTypeId}")
     public CommonResult<List> listBusinessByOrderTypeId(@PathVariable("orderTypeId") Integer
@@ -26,13 +39,22 @@ public class BusinessController {
         return new CommonResult(200,"success",list);
     }
 
-    @GetMapping("/getBusinessById/{businessId}")
+    @RequestMapping("/getBusinessById/{businessId}")
     public CommonResult<Business> getBusinessById(@PathVariable("businessId") Integer
                                                           businessId) throws Exception{
+
         Business business = businessService.getBusinessById(businessId);
-        //在商家微服务中调用食品微服务
+//        //通过服务提供者名（food-server）获取Eureka Server上的元数据
+//        List<ServiceInstance> instanceList = discoveryClient.getInstances("food-server");
+//        //现在，元数据集合中只有一个服务信息（food-server）
+//        ServiceInstance instance = instanceList.get(0);
+
+
+
+        //使用DiscoveryClient获取元数据，主机地址与端口就可以不硬编码了
         CommonResult<List> result =
-                restTemplate.getForObject("http://localhost:10200/FoodController/listFoodByBusinessId/"+businessId, CommonResult.class);
+                foodFeignClient.listFoodByBusinessId(businessId);
+        System.out.println(result.getMessage());
         if(result.getCode()==200) {
             business.setFoodList(result.getResult());
         }
---
+++ note.md
@@ -0,0 +1,30 @@
+1.父工程pom.xml文件里对artifactId的命名和微课不同，不匹配的命名导致项目报错 
+
+2.第一次运行报错： 
+
+Failed to bind properties under 'logging.level' to java.util.Map<java.lang.String, org.springframework.boot.logging.LogLevel>
+
+原因：是由application.yml文件的缩进问题导致的。对相应行缩进即可。
+
+3.启动成功后第一次测试Url地址失败，没有返回用户信息。 
+在这一步总共遇到了3个问题。 
+
+（1）课件提供的测试用url地址有误。按照课件的步骤，正确的地址应该是：http://localhost:10100/UserController/getUserByIdByPass/12345671111/123。
+
+（2）数据库权限问题。报错”Client does not support authentication protocol requested by server; consider upgrading MySQL clien“。authentication protocol即身份验证协议。修改mysql的权限即可。
+
+（3）数据库版本问题。解决耗时最长的问题。这个问题在之前的项目里也遇到过，当时只需要把pom.xml文件里的mysql-connector-java依赖从较低的版本升级到和电脑数据库版本匹配的版本即可。
+但是这次修改后却还是报相同的错。最终发现是因为maven包没有重新导入，导致项目启动时一直默认用低版本的依赖包。
+
+4.模块间的同名类冲突
+ 
+拷贝pom.xml给每个模块的时候没有修改相应的artifactId，导致多个模块被认为是同一个模块。需要重新创建工程。
+
+5.购物车微服务报错
+
+Invalid bound statement (not found): com.neusoft.mapper.CartMapper.listCart 原因：mapper.java的映射文件mapper.xml没拷贝过来
+
+
+无法更新购物车。原因：因为之前的Bug和误操作，数据库里针对某一个食品有多条购物车数据，导致数据紊乱。
+  
+6.Failed to convert value of type 'java.lang.String' to required type 'java.lang.Double'
